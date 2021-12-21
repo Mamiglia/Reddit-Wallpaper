1472ddfbde30d81549a4f4a05fee352209797bac
Iinfragon <95660369+Iinfragon@users.noreply.github.com>
2021-12-21 10:53:26 +1000

    Update GetNewWallpaper.java
    New class variables:
    int screens: physical screens
    boolean diff: if different images should be used per screen
    Set<Wallpaper> results: same as Wallpaper result but for handling multiple screens
    
    Adjusted GetNewWallpaper method:
    Format is now GetNewWallpaper(Settings settings, int screens, boolean diff)
    Added class variable assignment of screens and diff
    
    Adjusted call to Selector to include screens and diff
    
    Adjusted call to Selector.getResult to handle multi monitor supports

--- Utils/GetNewWallpaper.java
+++ Utils/GetNewWallpaper.java
@@ -14,18 +14,23 @@ public class GetNewWallpaper implements Runnable {
 	private static final Logger log = DisplayLogger.getInstance("Get New Wallpaper");
 	private final Settings settings;
 	public static final Wallpaper ERROR_VALUE = null;
+	private int screens;
+	private boolean diff;
 
 	private Wallpaper result;
+	private Set<Wallpaper> results;
 
-	public GetNewWallpaper(Settings settings) {
+	public GetNewWallpaper(Settings settings, int screens, boolean diff) {
 		this.settings = settings;
+		this.screens = screens;
+		this.diff = diff;
 	}
 
 
 	@Override
 	public void run() {
 		if (executed) return;
-		executed = true;
+		executed = true;;
 
 		Searcher s = new Searcher(settings);
 		s.generateSearchQuery();
@@ -40,7 +45,7 @@ public class GetNewWallpaper implements Runnable {
 		Selector selector;
 
 		try {
-			selector = new Selector(wallpapers, settings.doKeepWallpapers(), settings.getMaxDatabaseSize());
+			selector = new Selector(wallpapers, settings.doKeepWallpapers(), settings.getMaxDatabaseSize(), screens, diff);
 		} catch (IOException e) {
 			log.log(Level.SEVERE, "Loading DB is impossible. Aborting wallpaper set up");
 			abort();
@@ -48,22 +53,39 @@ public class GetNewWallpaper implements Runnable {
 		}
 
 		selector.run();
-		result = selector.getResult();
-
-		if (result != null && !result.isDownloaded()) {
-			try {
-				result.download();
-			} catch (IOException e) {
-				log.log(Level.SEVERE, "Couldn't download the image");
-				log.log(Level.FINER, e.getMessage());
+		if (!diff || screens == 1) {
+			result = selector.getResult();
+			if (result != null) {
+				if (!result.isDownloaded()) {
+					try {
+						result.download();
+					} catch (IOException e) {
+						log.log(Level.SEVERE, "Couldn't download the image");
+						log.log(Level.FINER, e.getMessage());
+					}
+				}
+				log.log(Level.FINER, () -> "GetNewWallpaper selected:\n" + result);
 			}
-		} else if (result == null){
+		} else if (screens > 1) {
+			results = selector.getResult(screens);
+			if (results != null) {
+				for (Wallpaper r : results) {
+					if (!r.isDownloaded()) {
+						try {
+							r.download();
+						} catch (IOException e) {
+							log.log(Level.SEVERE, "Couldn't download the image");
+							log.log(Level.FINER, e.getMessage());
+						}
+					}
+					log.log(Level.FINER, () -> "GetNewWallpaper selected:\n" + r);
+				}
+			}
+		}
+		if (result == null && results == null){
 			log.log(Level.SEVERE, "The selection process found no wallpaper");
 			abort();
-			return;
 		}
-
-		log.log(Level.FINER, () -> "GetNewWallpaper selected:\n" + result);
 	}
 
 	private void abort() {
@@ -75,8 +97,19 @@ public class GetNewWallpaper implements Runnable {
 		if (!executed) {
 			log.log(Level.INFO, "Result was requested but the functor was never executed");
 		} else if (result == null) {
-			log.log(Level.INFO, "No wallpaper was selected");
+			log.log(Level.INFO, "No wallpaper was selected single");
 		}
 		return result;
+	}
+
+	public Set<Wallpaper> getResult(int i) {
+		if (!executed) {
+			log.log(Level.INFO, "Result was requested but the functor was never executed");
+		} else if (results == null) {
+			log.log(Level.INFO, "No wallpaper was selected multi");
+		} else if (results.size() < i) {
+			log.log(Level.INFO, "Not enough wallpapers found for your screens");
+		}
+		return results;
 	}
 }
