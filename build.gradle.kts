ba038fc6c750a541262b2d59bb2180d9c0e361b4
Mamiglia <33762284+Mamiglia@users.noreply.github.com>
2022-07-07 20:43:17 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val javaVersion = JavaVersion.VERSION_11
+val jarName = "Reddit-Wallpaper.jar"
 
 
 plugins {
@@ -70,13 +71,20 @@ tasks {
     val fatJar = register<Jar>("fatJar") {
         dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
         archiveClassifier.set("standalone") // Naming the jar
+        archiveFileName.set(jarName)
+        destinationDirectory.set(File("out/artifacts/Reddit_Wallpaper_Gradle"))
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+        manifest { attributes["Main-Class"] = application.mainClass } // Provided we set it up in the application plugin configuration
+        manifest { attributes["Multi-Release"] = "true" }
         val sourcesMain = sourceSets.main.get()
         val contents = configurations.runtimeClasspath.get()
             .map { if (it.isDirectory) it else zipTree(it) } +
                 sourcesMain.output
         from(contents)
+        from({
+            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+        })
+        exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA")
     }
     build {
         dependsOn(fatJar) // Trigger fat jar creation during build
